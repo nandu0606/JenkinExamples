1c5b7f63bb47c015441d1db39b354160bd42f0f6
nandu0606 <133966833+nandu0606@users.noreply.github.com>
2025-03-29 12:32:20 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,14 @@
+class CommandLineDemo2{
+public static void main(String args[])
+{
+String name=args[0];
+int rollno=Integer.parseInt(args[1]);
+int age=Integer.parseInt(args[2]);
+String branch=args[3];
+
+System.out.println("name of student is:"+name);
+System.out.println("roll no  of student is:"+rollno);
+System.out.println("age:"+age);
+System.out.println("branch:"+branch);
+}
+}
